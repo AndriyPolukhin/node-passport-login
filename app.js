893ea4846f41b1db0c8c9ea6b1621841fe961763
AndriyPolukhin <andriy.polukhin@gmail.com>
2020-07-19 10:04:03 +0300

    added views

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// * DB config
+const db = require('./config/keys').MongoURI;
+
+// * ejs
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// * Routers
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server starter on ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    'mongodb+srv://passport-1234:passport-1234@cluster0.kyebf.mongodb.net/<dbname>?retryWrites=true&w=majority',
+};
